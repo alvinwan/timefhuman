07b5de12a44af2ba417f71e9fc831e1caa1a48f2
Alvin Wan <alvinwan1@gmail.com>
2025-01-24 21:27:37 -0800

    update grammar to never prefix a word

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -169,6 +169,8 @@ def test_custom_config(now, config, test_input, expected):
     ('September 30, 2019.', [('September 30, 2019', datetime.datetime(2019, 9, 30, 0, 0))]), # gh#26
     ('How does 5p mon sound? Or maybe 4p tu?', [('5p mon', datetime.datetime(2018, 8, 6, 17, 0)), ('4p tu', datetime.datetime(2018, 8, 7, 16, 0))]),
     ('There are 3 ways to do it', []),  # '3' should remain ambiguous and then be ignored
+    ('setup', []),  # no date or time (contains 'tu' but in another word)
+    ('9 amtrak', []),  # no date or time (contains 'am')
     # TODO: get matched characters gh#9
 ])
 def test_with_random_text(now, test_input, expected):
