b4bf489553d384a97b5ad143005f9503aa3b3a0e
Alvin Wan <alvinwan@users.noreply.github.com>
2025-02-22 23:06:03 -0800

    drop unused PyTest cmd class from setup.py

--- setup.py
+++ setup.py
@@ -6,24 +6,6 @@ tests_require = ['pytest==8.3.4', 'pytest-cov==6.0.0', 'coverage==7.6.10', 'cove
 install_requires = ['lark==1.2.2', 'babel==2.16.0', 'pytz==2024.2', 'python-dateutil==2.9.0.post0']
 
 
-class PyTest(TestCommand):
-
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
 VERSION = '0.1.2'
 
 setup(
