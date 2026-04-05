491967f9457d0135d82e6d47345086810cf54c38
Alvin Wan <alvinwan@users.noreply.github.com>
2026-04-05 04:39:04 -0700

    Bump version to 0.1.5 (#108)

--- timefhuman/__init__.py
+++ timefhuman/__init__.py
@@ -2,5 +2,5 @@ from .main import DEFAULT_CONFIG, timefhuman
 from .utils import Direction, tfhConfig
 
 
-__VERSION__ = '0.1.4'
+__VERSION__ = '0.1.5'
 __all__ = ("timefhuman", "tfhConfig", "Direction", "DEFAULT_CONFIG")
